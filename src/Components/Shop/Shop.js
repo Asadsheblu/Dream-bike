468569bc0367863c73584f63b4f4e44bd5ccdfd2
Asadsheblu <shebluasad@gmail.com>
2022-03-30 21:25:10 +0600

    localstorage -add

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -4,10 +4,10 @@ import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from './Product/Product';
 import './Shop.css'
-// const cartFromLocalStorage=JSON.parse(localStorage.getItem('carts','[]'))
+const cartFromLocalStorage=JSON.parse(localStorage.getItem('carts','[]'))
 const Shop = () => {
     const [products,setProducts]=useState([])
-    const [carts,setCarts]=useState([])
+    const [carts,setCarts]=useState(cartFromLocalStorage)
     
     useEffect(()=>{
         fetch('bike-data.json')
@@ -15,10 +15,10 @@ const Shop = () => {
         .then(data=>setProducts(data))
     },[])
     //localstorage
-    // useEffect(()=>{
-    //     localStorage.setItem('carts',JSON.stringify(carts));
+    useEffect(()=>{
+        localStorage.setItem('carts',JSON.stringify(carts));
 
-    // },[carts])
+    },[carts])
     //addtocart function
     const addToCart=(product)=>{
         const newCart=[...carts,product]
